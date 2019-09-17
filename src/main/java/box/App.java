a7f33a379a05bcc0acb14efa63473d02a1dd8974
Imaraika <inange2013@gmail.com>
2019-09-17 16:25:55 +0200

    changes have been added

---
+++ src/main/java/box/App.java
@@ -0,0 +1,111 @@
+package box;
+
+import box.Hero;
+import box.Squard;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args){
+        staticFileLocation("/public");
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        }else {
+            port = 4567;
+        }
+        port(port);
+
+        staticFileLocation("/public");
+        Hero.newHeroSet();
+        Squard.setUpNewSquad();
+//       Squad.setUpNewSquad1();
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model,"index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/hero-form",(req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "heroPage.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/hero",(req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Hero> hero = Hero.getAllInstances();
+            model.put("hero",hero);
+            return new ModelAndView(model, "hero.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/squad-form",(req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "squadform.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/squad",(req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Squard> squads = Squard.getInstances();
+            model.put("squads",squads);
+            ArrayList<Hero> members = Hero.getAllInstances();
+            model.put("heroes",members);
+            Squard newSquad = Squard.findBySqId(1);
+            model.put("allSquadMembers", newSquad.getSquadMembers());
+            return new ModelAndView(model, "squad.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/squad/new",(req,res)-> {
+            Map<String, Object> model = new HashMap<>();
+            String squadName = req.queryParams("squadName");
+            Integer size = Integer.parseInt(req.queryParams("size"));
+            String cause = req.queryParams("cause");
+            Squard newSquad = new Squard(squadName,size,cause);
+            req.session().attribute("item",squadName);
+            model.put("item",req.session().attribute("item"));
+            return new ModelAndView(model,"success.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/new/hero",(req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+            String name = req.queryParams("name");
+            Integer age = Integer.parseInt(req.queryParams("age"));
+            String power = req.queryParams("power");
+            String weakness = req.queryParams("weakness");
+            Hero newHero = new Hero(name,age,power,weakness);
+            req.session().attribute("item",name);
+            model.put("item",req.session().attribute("item"));
+            model.put("newHero",newHero);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/new/member/:squadId",(req,res)->{
+            Map<String, Object> model = new HashMap<>();
+            req.session().attribute("selectedSquad",req.params("squadId"));
+            model.put("selectedSquad", req.session().attribute("selectedSquad"));
+            model.put("item",1);
+            return new ModelAndView(model, "success.hbs");
+        },new HandlebarsTemplateEngine());
+
+        get("/squad/new/:id",(req,res)->{
+            Map<String, Object> model = new HashMap<>();
+            int id= Integer.parseInt(req.params(":id"));
+            Hero newMember = Hero.findById(id);
+            Squard newSquad = Squard.findBySqId(1);
+            newSquad.setSquadMembers(newMember);
+            model.put("item", newMember.getName());
+            model.put("newHero",newSquad.getSquadName());
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+    }
+}
---
+++ src/main/java/box/Squard.java
@@ -0,0 +1,62 @@
+package box;
+
+import java.util.ArrayList;
+
+public class Squard {
+    public int SqId;
+    public String name_of_Squad;
+    public int maximum_number;
+    public String squardcause;
+
+    private static ArrayList<box.Hero> squardIncluders= new ArrayList<>();
+    public  static ArrayList<Squard> instances = new ArrayList<>();
+
+     public Squard(String name_of_Squad ,int maximum_number, String cause){
+         this.name_of_Squad = name_of_Squad;
+         this.maximum_number = maximum_number;
+         this.squardcause = cause;
+         this.squardIncluders = new ArrayList<>();
+         instances.add(this);
+         this.SqId = instances.size();
+     }
+
+    public int getSqId() {
+         return SqId;
+     }
+    public static Squard findBySqId(int id) {
+         return instances.get(id-1);
+     }
+    public String getSquadName() {
+         return name_of_Squad;
+     }
+    public int getSize() {
+         return maximum_number;
+     }
+    public String getSqdCause() {
+         return this.squardcause;
+     }
+    public static ArrayList<Squard> getInstances(){
+         return instances;
+     }
+    public ArrayList<box.Hero> getSquadMembers(){
+        return  squardIncluders;
+    }
+    public void setSquadMembers(box.Hero newMember) {
+        squardIncluders.add(newMember);
+    }
+    public static void clearAllSquads(){
+         instances.clear();
+     }
+    public void clearAllSquadMembers(){
+         getSquadMembers().clear();
+     }
+
+    public static Squard setUpNewSquad(){
+         return new Squard("Nyabugogo Data Promoters ",5,"data promoting");
+     }
+    public static Squard setUpNewSquad1(){
+         return new Squard("Kicukiro Data Promoters",5,"data promoting");
+     }
+
+}
+
